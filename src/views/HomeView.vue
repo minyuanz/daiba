b93a883a2ad39cf2321e8b7ac7924392de7c5e94
Xiaogozui <dilendhuang2021@gmail.com>
2023-09-19 09:50:22 +0800

    首頁動畫

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="homeWrapper">
-    <div class="animateLineGroup">
+  <div class="animateLineGroup">
         <div class="left">
           <div class="thick"></div>
           <div class="dark1 slim"></div>
@@ -25,7 +25,7 @@
           <div class="dark1 slim"></div>
           <div class="thick"></div>
         </div>
-    </div>
+  </div>
   <nav class="homeNav">
     <ul>
       <li>
@@ -88,61 +88,125 @@
   </nav>
 
   <div class="homeBanner">
-    <div class="swiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=1" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=1" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=1" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=2" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=2" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=2" alt="" />
+      <div class="swiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=1" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=1" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=1" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=2" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=2" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=2" alt="" />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="logo">
-      <img src="../../public/img/logo.png" alt="logo" />
-    </div>
-    <div class="swiper mb-none">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=1" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=1" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=1" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=2" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=2" alt="" />
-        </div>
-        <div class="swiper-slide">
-          <img src="https://picsum.photos/200/300/?random=2" alt="" />
+      <div class="logo">
+        <img src="../../public/img/logo.png" alt="logo" />
+      </div>
+      <div class="swiper mb-none">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=1" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=1" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=1" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=2" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=2" alt="" />
+          </div>
+          <div class="swiper-slide">
+            <img src="https://picsum.photos/200/300/?random=2" alt="" />
+          </div>
         </div>
       </div>
     </div>
   </div>
+  <div class="animate">
+    <div class="circle">
+      <svg
+        ref="svgElement"
+        xmlns="http://www.w3.org/2000/svg"
+        width="40%"
+        height="40%"
+        viewBox="0 0 107 190"
+        enable-background="new 0 0 107 190"
+      >
+        <circle
+          fill="none"
+          id="circle"
+          stroke-dasharray="0 610 610 0"
+          class="stat-outline-circle"
+          stroke="#fff04b"
+          stroke-width="8"
+          stroke-miterlimit="10"
+          cx="53"
+          cy="56"
+          r="48"
+        ></circle>
 
-</div>
+        <animate
+          attributeType="XML"
+          xlink:href="#circle"
+          id="circleanimate"
+          attributeName="stroke-dashoffset"
+          from="0"
+          to="610"
+          dur="1s"
+          begin=".5s"
+          fill="freeze"
+        />
+
+        <!-- <animate
+          attributeType="XML"
+          xlink:href="#circle"
+          id="circleanimate2"
+          attributeName="stroke-dashoffset"
+          from="610"
+          to="0"
+          dur="1s"
+          begin="1.5s"
+          fill="freeze"
+        />   -->
+      </svg>
+    </div>
+
+    <div class="animateLine">
+      <div class="row-line">
+        <div class="r-line chocolate"></div>
+        <div class="r-line success"></div>
+        <div class="r-line danger"></div>
+      </div>
+
+      <div class="col-line">
+        <div class="c-line info"></div>
+        <div class="c-line tangerine"></div>
+        <div class="c-line grap"></div>
+      </div>
+
+    </div>
+  </div>
 
 
 </template>
 
 <script>
+import gsap from "gsap";
 import Swiper from "swiper/bundle";
 export default {
 
@@ -161,84 +225,116 @@ export default {
         disableOnInteraction: false,
       },
     });
+    setTimeout(() => {
+      const circleElement = document.querySelector(".animate");
+      if (circleElement) {
+        circleElement.style.display = "none";
+      }
+    }, 4300);
+
+    setTimeout(() => {
+      const animateElement = document.querySelector(".animate");
+      if (animateElement) {
+        animateElement.classList.add("fadeOut");
+      }
+    }, 2500);
+
+      
+    gsap.fromTo(
+      ".chocolate",
+      {
+        x: "150%",
+      },
+      {
+        x: "-150%",
+        duration: 1,
+        delay: 0,
+      }
+    );
+
+    gsap.fromTo(
+      ".success ",
+      {
+        x: "150%",
+      },
+      {
+        x: "-150%",
+        duration: 1,
+        delay: .2,
+      }
+    );
+
+    gsap.fromTo(
+      ".danger",
+      {
+        x: "150%",
+      },
+      {
+        x: "-150%",
+        duration: 1,
+        delay: .4,
+      }
+    );
+
+    gsap.fromTo(
+      ".info ",
+      {
+        y: "150%",
+      },
+      {
+        y: "-160%",
+        duration: 1,
+        delay:.6,
+      }
+    );
+
+    gsap.fromTo(
+      ".tangerine  ",
+      {
+        y: "150%",
+      },
+      {
+        y: "-160%",
+        duration: 1,
+        delay: .8,
+      }
+    );
+
+    gsap.fromTo(
+      ".grap   ",
+      {
+        y: "150%",
+      },
+      {
+        y: "-160%",
+        duration: 1,
+        delay: 1,
+      }
+    );
   },
 };
 </script>
+<!-- <script>
+import gsap from "gsap"; // 确保正确引入GSAP库
 
-<style>
-.homeWrapper{
-  position: relative;
-  max-width: 100%;
-  height: 100vh;
-  overflow: hidden;
-}
-.animateLineGroup{
-    width: 100%;
-    display: flex;
-    justify-content: space-between;      
-    position: relative;
-    position: absolute;
-    z-index: -1;
-    /* overflow: hidden; */
-    .left{
-        width: 18.75%;
-        display: flex;
-        justify-content: space-between;
-        position: absolute;
-        animation: fadeInLeft 5.5s ease-out 0s ;
-      }
-    .right{
-      width: 18.75%;
-        display: flex;
-        right: 0;
-        justify-content: space-between;
-        position: absolute;
-        animation: fadeInRight 5.5s ease-out 0s ;
+export default {
+  mounted() {
+    // 在组件挂载后初始化GSAP动画
+    gsap.set(".box2", { transformOrigin: "50% 50%" });
+
+    // 创建一个无限循环的动画
+    function bounceImage() {
+      gsap.to(".box2", {
+        x: () => gsap.utils.random(0, 1000, 60), // 随机水平位移
+        y: () => gsap.utils.random(0, 200, 5), // 随机垂直位移
+        rotation: () => gsap.utils.random(-45, 45), // 随机旋转角度
+        duration: 2, // 持续时间
+        ease: "power2.inOut",
+        onComplete: bounceImage, // 完成后再次调用此函数
+      });
     }
-        .thick{
-          width: 20%;
-          height: 100vh;
-          background-color: #000;
-        }
-        .slim{
-          width: 6%;
-        }
-        .dark1{
-          background-color: #1A1A1A;
-        }
-        .dark2{
-          background-color: #323232;
-        }
-        .dark3{
-          background-color: #4C4C4C;
-        }
-        .dark4{
-          background-color: #666666;
-        }
-        .dark5{
-          background-color: #808080;
-        }
-        .dark6{
-          background-color: #9A9A9A;
-        }
-        .dark7{
-          background-color: #B4B4B4;
-        }
-        .dark8{
-          background-color: #CCCCCC;
-        }
-        .dark9{
-          background-color: #E6E6E6;
-        }
 
-  }
-@keyframes fadeInLeft{
-    0%{ left: -100%;}
-  100%{ left: 0;}
-}
-@keyframes fadeInRight{
-    0%{ right: -100%;}
-  100%{ right: 0;}
-}
-
-</style>
+    bounceImage(); // 开始动画
+  },
+};
+</script> -->
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <router-link to="/home" class="store">前台首頁</router-link>
         <button>前台</button>
         <button>後台</button>
     </div>
