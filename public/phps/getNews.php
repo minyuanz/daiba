40c772a9db7213c7943a1955d60e505803fa768b
minyuan <a1097562@gmail.com>
2023-10-01 20:33:47 +0800

    消息館立v1

---
+++ public/phps/getNews.php
@@ -0,0 +1,22 @@
+<?php
+header("Access-Control-Allow-Origin:*"); // 允许任何来源访问
+
+// header("Content-Type: application/json");
+
+
+try {
+    // 連接資料庫
+    require_once("connect_chd103g5.php");
+    // sql指令
+	$sql = "select * from news";
+    // 
+    $news = $pdo->query( $sql );
+    $result=$news->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo json_encode($result);
+
+} catch (PDOException $e) {
+	$result = ["error" => true, "msg" => $e->getMessage()];
+	echo json_encode($result);
+}
+?>
