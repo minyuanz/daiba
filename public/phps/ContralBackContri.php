c634e10a377bcda31f5ef9edbec704d548d2d4d6
gimmy1013 <gimmy1013@gmail.com>
2023-10-17 12:58:40 +0800

    更新一些狀態bug

--- public/phps/ContralBackContri.php
+++ public/phps/ContralBackContri.php
@@ -8,14 +8,14 @@ require_once("connect_chd103g5_2.php");
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $data = json_decode(file_get_contents("php://input"));
-    if (isset($data->art_no, $data->art_status)) {
-        $art_no = $data->art_no;
+    if (isset($data->art_id, $data->art_status)) {
+        $art_id = $data->art_id;
         $art_status = $data->art_status;
 
-        $sql = "UPDATE article SET art_status = :art_status WHERE art_no = :art_no";
+        $sql = "UPDATE article SET art_status = :art_status WHERE art_id = :art_id";
         $stmt = $pdo->prepare($sql);
         $stmt->bindParam(':art_status', $art_status, PDO::PARAM_STR);
-        $stmt->bindParam(':art_no', $art_no, PDO::PARAM_INT);
+        $stmt->bindParam(':art_id', $art_id, PDO::PARAM_INT);
         $stmt->execute();
 
         if ($stmt->rowCount() > 0) {
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $response = array("success" => false, "msg" => "更新失败");
         }
     } else {
-        $response = array("success" => false, "msg" => "參數 art_no 和 art_status 遺失");
+        $response = array("success" => false, "msg" => "參數 art_id 和 art_status 遺失");
     }
 } else {
     header('HTTP/1.1 200 OK');
