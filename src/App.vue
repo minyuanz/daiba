b5573830e4c5e5c6af6d54949cbe326bd873ae37
mochi_nino <z898911z@gmail.com>
2023-10-19 18:43:20 +0800

    後台投稿彈窗跑版

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ export default {
   },
   mounted() {
     fetch(
-      "https://api.weatherapi.com/v1/current.json?q=Taipei&lang=zh_tw&key=831993a5339d4b7cadc74621231609"
+      "https://api.weatherapi.com/v1/current.json?q=Taipei&lang=zh_tw&key=6270d6c5f0ba491196f45945231810"
     )
       .then((res) => res.json())
       .then((json) => {
